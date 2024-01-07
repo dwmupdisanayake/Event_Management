caaa25190e7ef9f3308a9c29db8f1fb19e422931
dwmupdisanayake <149526768+dwmupdisanayake@users.noreply.github.com>
2024-01-07 19:10:10 +0530

    Add files via upload

---
+++ footer/header.php
@@ -0,0 +1,36 @@
+ <!-- nav bar start -->
+ <div class="row">
+            <div class="col-md-12">
+                <nav class="navbar navbar-expand-lg bg-body-tertiary  navbar">
+                    <div class="container">
+                        <a class="navbar-brand" href="#">
+                            <img src="asset/logo/Colorful Creative Personal Brand Logo 2.png" width="30px" height="30px" alt="" srcset="">
+                            <span style="color: white; margin-left: 10px;">Eventra</span>
+                        </a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"><i class="fa-sharp fa-solid fa-bars" style="color: #e9ecf1;"></i></span>
+                      </button>
+                        <div class="collapse navbar-collapse navbar-text" id="navbarSupportedContent">
+                            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="home.html">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="aboutus.html">About Us</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="event.html">Event</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="photVideo.html">Photo/Video</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="buyTicket.html">Buy Tickets</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    <!-- navbar end -->
